261a1f1d640e8668a0901fd2d0407a1a0a2f095d
Brian P. Holt <bholt@dwolla.com>
2015-07-14 10:22:14 -0500

    specify http in metadata request

--- registrator_on_ec2_hostname.sh
+++ registrator_on_ec2_hostname.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export HOST=$(curl -s 169.254.169.254/latest/meta-data/local-hostname)
-export LOCAL_IP=$(curl -s 169.254.169.254/latest/meta-data/local-ipv4)
-registrator consul://$(curl -s 169.254.169.254/latest/meta-data/local-ipv4):8500
+export HOST=$(curl -s http://169.254.169.254/latest/meta-data/local-hostname)
+export LOCAL_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+registrator consul://$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4):8500
 
