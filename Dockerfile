349d501667d8b6882d149a7111498581801fc186
Brian P. Holt <bholt@dwolla.com>
2019-03-14 16:36:16 -0500

    trap when registrator shuts down and explicitly deregister any containers running on this instance

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM gliderlabs/registrator:v7
-MAINTAINER Dwolla Platform Team
-ADD registrator_on_ec2_hostname.sh /usr/local/bin/registrator_on_ec2_hostname.sh
-RUN apk-install curl
+LABEL maintainer="Dwolla Platform Team"
+COPY registrator_on_ec2_hostname.sh /usr/local/bin/registrator_on_ec2_hostname.sh
+RUN apk-install bash curl jq
 ENTRYPOINT ["registrator_on_ec2_hostname.sh"]
--- registrator_on_ec2_hostname.sh
+++ registrator_on_ec2_hostname.sh
@@ -1,4 +1,19 @@
-#!/bin/sh
-export LOCAL_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-echo Running: registrator -ip $LOCAL_IP $@ consul://$LOCAL_IP:8500
-exec registrator -ip $LOCAL_IP "$@" consul://$LOCAL_IP:8500
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+IFS=$'\n\t'
+
+function deregister {
+  registrator_exit_code=$?
+  echo "Registrator has exited; cleaning up containers registered on this instance" > /dev/stderr
+  set -o xtrace
+  curl --silent "http://${LOCAL_IP}:8500/v1/agent/services" | \
+    jq -r "map(\"http://${LOCAL_IP}:8500/v1/agent/service/deregister/\(.ID)\") | .[]" | \
+    xargs curl --silent --verbose -XPUT
+  exit ${registrator_exit_code}
+}
+trap deregister EXIT
+
+LOCAL_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+export LOCAL_IP
+echo "Running: registrator -ip ${LOCAL_IP} $* consul://$LOCAL_IP:8500" > /dev/stderr
+registrator -ip "${LOCAL_IP}" "$@" "consul://${LOCAL_IP}:8500"
